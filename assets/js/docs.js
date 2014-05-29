7a99d897e763c44f2cb89278f6bc754f8901cc27
Konduto <dev@konduto.com>
2014-05-29 16:38:29 -0300

    Page tagging, cleaner layout, i18n

---
+++ assets/js/docs.js
@@ -0,0 +1,35 @@
+/* i18next */
+i18n.init({
+  fallbackLng:'en',
+  useLocalStorage:false,
+  useCookie:false,
+  debug:true,
+  detectLngQS:'lang'
+}, function(t) {
+  i18nlang = i18n.lng().substring(0,2);
+  $("body").i18n();
+  /*$(".navbar").i18n();
+  $("#content").i18n();
+  $(".container_footer").i18n();*/
+  $('#flag--active--'+i18nlang).removeClass('hidden');
+  $('li[id^="flag--"]').removeClass('is-active');
+  $('#flag--'+i18nlang).addClass('is-active');
+});
+
+/* Generate pseudo-random order id number for code examples */
+$(document).ready(function() {
+  var order = (new Date).getTime();
+  var i = Math.floor((Math.random()*9879870)+1);
+  $('span[id^="order_id_"]').each(function() {
+    var x = order+i;
+    $(this).append('"'+x+'",');
+    i++;
+  });
+});
+
+/* Toggles code language */
+$('button[id^="toggle-"]').click(function() {
+  $('div[id^="example-"]').attr('class', 'hidden');
+  var x = $(this).attr("id").replace("toggle-","example-")+"-";
+  $('div[id^='+x+']').attr('class', 'visible');
+})
